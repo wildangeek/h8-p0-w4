b8bb6c2c9a3401cbdb515055d357b59771ca97b9
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-06-05 06:52:00 +0700

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,22 @@
+/* Problem: Diberikan sebuah function angkaPrima(angka) yang menerima satu parameter berupa angka. Function akan me-return true jika angka tersebut adalah bilangan prima. Jika tidak, return false. */
 
+function angkaPrima(angka){
+  var jumlahBagi =0
+  for(var i =0; i<=angka ; i++){
+    if(angka %i===0){
+      jumlahBagi++
+    }
+  }
+  if(jumlahBagi==2){
+    return true
+  }else{
+    return false
+  }
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(2)); // false
